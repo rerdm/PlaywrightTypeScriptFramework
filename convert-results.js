cb9fca816d21f6b1f3b2422426f710c4768e987e
rerdm <reneerdmann87@web.de>
2025-10-01 16:27:47 +0200

    create convert.js file to show resulst in mail

--- convert-results.js
+++ convert-results.js
@@ -5,13 +5,13 @@ const filePath = path.join(__dirname, 'test-results', 'failed-tests.json');
 
 const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
-let output = '';
+let output = '\n';
 
 data.forEach(test => {
   const result = test.result.padEnd(10);
-  const testName = test.testName.padEnd(59);
+  const testName = test.testName.padEnd(60);
   const specName = test.specName; 
-  output += `${result} - ${testName} - ${specName}\n`;
+  output += `${result} -     ${testName} -     ${specName}\n`;
 });
 
 const outputFilePath = path.join(__dirname, 'test-results', 'formatted-results.txt');
